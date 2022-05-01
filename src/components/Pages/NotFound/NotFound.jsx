d7d13abcc877d6907ba8e890f58da2b9e13f8e67
rockydey <coderrocky25@gmail.com>
2022-05-01 18:27:09 +0600

    404 page complete

--- src/components/Pages/NotFound/NotFound.jsx
+++ src/components/Pages/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './NotFound.css';
+import notFound from '../../../images/404.png';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>This is Not Found Page</h1>
+        <div className='flex justify-center my-10'>
+            <img src={notFound} className="w-5/12 rounded-xl" alt="" />
         </div>
     );
 };
