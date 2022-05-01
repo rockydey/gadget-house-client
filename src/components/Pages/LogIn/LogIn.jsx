4eea1936bad96c4b7a637c70d09076c850a0508e
rockydey <coderrocky25@gmail.com>
2022-05-01 21:20:53 +0600

    login page authentication complete

--- src/components/Pages/LogIn/LogIn.jsx
+++ src/components/Pages/LogIn/LogIn.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LogIn.css';
 import Google from '../../../images/google.png';
-import { useNavigate } from 'react-router-dom';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const LogIn = () => {
-    const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [signInWithGoogle, user1] = useSignInWithGoogle(auth);
+    const [signInWithEmailAndPassword, user2, error,] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+
     const navigate = useNavigate();
+    let location = useLocation();
+    let from = location.state?.from?.pathname || "/home";
+
+    const handleEmail = event => {
+        setEmail(event.target.value);
+    };
+    const handlePassword = event => {
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    };
+
     const switchSignUp = () => {
         navigate('/signup');
     };
+
+    const resetPassword = async () => {
+        await sendPasswordResetEmail(email);
+        toast("Reset Password Email Sent!");
+    };
+
+    if (user1 || user2) {
+        navigate(from, { replace: true });
+    }
     return (
         <div className='login p-6 mx-auto my-14 rounded-xl'>
             <h1 className='text-center text-lg font-semibold mb-3'>To continue, log in to Gadget House.</h1>
@@ -24,13 +56,16 @@ const LogIn = () => {
                 <div className='line w-6/12'></div>
             </div>
 
-            <form className="login-form flex flex-col">
+            <form onSubmit={handleSubmit} className="login-form flex flex-col">
                 <label className='font-semibold mb-1' htmlFor="email">Email Address</label>
-                <input className='pr-10 py-2 pl-3 rounded-md mb-3' type="email" name="email" id="email" placeholder='Enter Your Email' required />
+                <input onBlur={handleEmail} className='pr-10 py-2 pl-3 rounded-md mb-3' type="email" name="email" id="email" placeholder='Enter Your Email' required />
                 <label className='font-semibold mb-1' htmlFor="password">Password</label>
-                <input className='pr-10 py-2 pl-3 rounded-md' type="password" name="password" id="password" placeholder='Enter Your Password' required />
+                <input onBlur={handlePassword} className='pr-10 py-2 pl-3 rounded-md' type="password" name="password" id="password" placeholder='Enter Your Password' required />
+                <p className='text-red-600'>
+                    {error?.message}
+                </p>
                 <div className='flex justify-between items-center mt-6'>
-                    <p className='text-base font-semibold'>Forget Your Password?</p>
+                    <p onClick={resetPassword} className='text-base font-semibold'>Forget Your Password?</p>
                     <input className='btn-submit text-white text-lg font-semibold px-8 py-2 rounded-full' type="submit" value="Login" />
                 </div>
             </form>
